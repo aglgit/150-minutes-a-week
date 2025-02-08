48b08a9755cbbcd6c8407a75484ad8f9035b8c0c
Andreas L <aglgit@users.noreply.github.com>
2025-02-09 00:17:19 +0100

    Add prettier

---
+++ apps/frontend/.prettierrc.mjs
@@ -0,0 +1,9 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ["prettier-plugin-tailwindcss"],
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+};
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import BaseLayout from "@/components/BaseLayout";
 import Calendar from "@/components/calendar";
 
 export default function Home() {
