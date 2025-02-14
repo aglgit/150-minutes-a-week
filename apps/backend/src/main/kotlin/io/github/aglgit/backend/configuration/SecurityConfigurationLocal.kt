b0724881cad7dc293944af0a90d8f597828fedc3
Andreas L <aglgit@users.noreply.github.com>
2025-02-14 09:20:49 +0100

    Use local profile

--- apps/backend/src/main/kotlin/io/github/aglgit/backend/configuration/SecurityConfigurationLocal.kt
+++ apps/backend/src/main/kotlin/io/github/aglgit/backend/configuration/SecurityConfigurationLocal.kt
@@ -2,6 +2,7 @@ package io.github.aglgit.backend.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -13,6 +14,7 @@ import org.springframework.web.cors.CorsUtils
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@Profile("local")
 class SecurityConfigurationLocal {
 
     @Bean
