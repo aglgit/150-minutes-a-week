e79a0ec9dfac25e15d948437a43b067bb155ebe8
Andreas L <aglgit@users.noreply.github.com>
2025-02-21 11:04:10 +0100

    Change DTO

--- apps/backend/src/main/kotlin/io/github/aglgit/backend/controllers/dtos/EventDto.kt
+++ apps/backend/src/main/kotlin/io/github/aglgit/backend/controllers/dtos/EventDto.kt
@@ -2,44 +2,32 @@ package io.github.aglgit.backend.controllers.dtos
 
 import io.github.aglgit.backend.services.domain.Activity
 import io.github.aglgit.backend.services.domain.Event
-import java.time.LocalDate
-import java.time.LocalTime
-import java.time.ZoneId
 import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
 data class EventDto(
     val id: Long,
     val userId: Long,
     val activity: Activity,
-    val date: String,
-    val startTime: String,
-    val endTime: String,
+    val startTime: ZonedDateTime,
+    val endTime: ZonedDateTime,
 )
 
 fun Event.toDto(): EventDto {
-    val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-    val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
-
     return EventDto(
         id = this.id,
         userId = this.userId,
         activity = this.activity,
-        date = this.startTime.format(dateFormatter),
-        startTime = this.startTime.format(timeFormatter),
-        endTime = this.endTime.format(timeFormatter)
+        startTime = this.startTime,
+        endTime = this.endTime
     )
 }
 
 fun EventDto.toEvent(): Event {
-    val date = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-    val startTime = LocalTime.parse(startTime, DateTimeFormatter.ofPattern("HH:mm"))
-    val endTime = LocalTime.parse(endTime, DateTimeFormatter.ofPattern("HH:mm"))
     return Event(
         id = this.id,
         userId = this.userId,
         activity = this.activity,
-        startTime = ZonedDateTime.of(date, startTime, ZoneId.of("UTC")),
-        endTime = ZonedDateTime.of(date, endTime, ZoneId.of("UTC"))
+        startTime = this.startTime,
+        endTime = this.endTime,
     )
 }
